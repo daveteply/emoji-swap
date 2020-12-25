9fe84e23419a3706133eb1491af848014d33657e
David Teply <davehamdan@gmail.com>
2020-12-25 06:22:35 -0700

    adding publish scripts

---
+++ publish.cmd
@@ -0,0 +1,8 @@
+git checkout main
+call ng build --prod --baseHref="https://daveteply.github.io/emoji-swap/demo/"
+git checkout gh-pages
+git pull
+git add demo*
+git commit -m "Updating demo"
+git push
+git checkout main
