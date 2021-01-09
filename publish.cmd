e83da52e65a06666e1e80b57540a53eda16ced50
David Teply <davehamdan@gmail.com>
2021-01-09 16:52:48 -0700

    updating publish script

--- publish.cmd
+++ publish.cmd
@@ -4,7 +4,10 @@ echo.
 git ls-tree --name-only -r gh-pages
 echo.
 
-set /p version="Enter version: (e.g. 0.2)"
+set /p version="Enter version(e.g. 0.2): "
 echo %version%
 
-rem ng build --prod --baseHref="https://daveteply.github.io/emoji-swap/demo/"
+@echo on
+ng build --prod --output-path demo-%version% --baseHref="https://daveteply.github.io/emoji-swap/demo-%version%/"
+
+@echo off
