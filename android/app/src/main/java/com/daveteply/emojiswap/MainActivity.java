ea6ba38bcd74207987f9f58230e4a92a2603646a
Dave Teply <davehamdan@gmail.com>
2022-04-10 15:45:34 -0700

    adding capacitor

---
+++ android/app/src/main/java/com/daveteply/emojiswap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.daveteply.emojiswap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.daveteply.emojiswap',
+  appName: 'emoji-swap',
+  webDir: 'dist/emoji-swap',
+  bundledWebRuntime: false
+};
+
+export default config;
