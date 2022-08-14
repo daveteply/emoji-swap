49fdd82227c8ac2fc74244bb77f176b5bf023f4c
Dave Teply <davehamdan@gmail.com>
2022-08-13 19:27:40 -0700

    reverting back to 7 rows for desktop/tablet

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
 export const APP_TITLE = 'Griddin';
 
 // default grid
-export const DEFAULT_GAME_BOARD_ROW_COUNT = 8;
+export const DEFAULT_GAME_BOARD_ROW_COUNT = 7;
 export const DEFAULT_GAME_BOARD_COLUMNS_COUNT = 5;
 
 // level
