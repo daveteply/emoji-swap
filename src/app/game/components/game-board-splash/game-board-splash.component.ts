2b88c758cedb30b61851e81dd8aa4c13c23ee0ea
David Teply <davehamdan@gmail.com>
2022-08-25 17:07:52 -0700

    "fixing" font issues in Safari

--- src/app/game/components/game-board-splash/game-board-splash.component.ts
+++ src/app/game/components/game-board-splash/game-board-splash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Device } from '@capacitor/device';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { GameSplash, GameSplashService, SplashType } from '../../services/game-splash.service';
@@ -23,7 +24,14 @@ export class GameBoardSplashComponent implements OnInit, OnDestroy {
 
   public splashType = SplashType;
 
+  public isIOS!: boolean;
+
   ngOnInit(): void {
+    // it is a shame I have to do this in 2022 to compensate for Safari's font handling
+    Device.getInfo().then((deviceInfo) => {
+      this.isIOS = deviceInfo.platform === 'ios';
+    });
+
     this.gameSplashService.gameSplashState$.pipe(takeUntil(this.subscription)).subscribe((scoreSplash: GameSplash) => {
       // remove expired elements
       this.splashItems = this.splashItems.filter(
